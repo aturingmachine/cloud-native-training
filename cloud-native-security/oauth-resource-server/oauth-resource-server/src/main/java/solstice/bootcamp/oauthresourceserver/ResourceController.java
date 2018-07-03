49a2d1467146d5fd0c3aa24eece9389ff389a2f6
Vincent Blom <blomvin@gmail.com>
2018-07-03 13:20:27 -0500

    OAuth Project working!

---
+++ cloud-native-security/oauth-resource-server/oauth-resource-server/src/main/java/solstice/bootcamp/oauthresourceserver/ResourceController.java
@@ -0,0 +1,13 @@
+package solstice.bootcamp.oauthresourceserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/")
+    public String helloSecured() {
+        return "Hello - Secured";
+    }
+}

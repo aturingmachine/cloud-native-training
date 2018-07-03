f682e391fe15efef8e507160424d4b556f57f2bd
Vincent Blom <blomvin@gmail.com>
2018-07-03 10:35:07 -0500

    Removes unused import in GreetingController
    This was leftover from my valiant battle with Spring 1.5 v Spring 2.0

--- cloud-native-actuator/src/main/java/solstice/bootcamp/springbootactuator/GreetingController.java
+++ cloud-native-actuator/src/main/java/solstice/bootcamp/springbootactuator/GreetingController.java
@@ -2,7 +2,6 @@ package solstice.bootcamp.springbootactuator;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Metrics;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 

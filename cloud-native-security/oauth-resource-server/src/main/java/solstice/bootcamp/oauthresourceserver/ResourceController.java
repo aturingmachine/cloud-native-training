469d9c3480016c7f9b760b1cd98ed072245e6db4
Vincent Blom <blomvin@gmail.com>
2018-07-03 13:42:24 -0500

    OAuth is now actually working
    I accidentally commited the broken version, this is the working version...

---
+++ cloud-native-security/oauth-resource-server/src/main/java/solstice/bootcamp/oauthresourceserver/ResourceController.java
@@ -0,0 +1,16 @@
+package solstice.bootcamp.oauthresourceserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/")
+    public String helloSecured() {
+
+        return "Hello - Secured";
+    }
+
+}
